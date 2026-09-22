c48afb0f1c1e32c0efe1bc38d158bbed9572777a
chetanjadhav8503-crj <chetanjadhav8503@gmail.com>
2026-09-22 11:22:10 +0530

    Update info.py

--- info.py
+++ info.py
@@ -13,10 +13,10 @@ def is_enabled(value, default):
 #---------------------------------------------------------------
 #---------------------------------------------------------------         ,
 SESSION = environ.get('SESSION', 'Media_search')
-API_ID = int(environ.get('API_ID', '21484'))
-API_HASH = environ.get('API_HASH', 'db6286a041adf07ef4244e')
-BOT_TOKEN = environ.get('BOT_TOKEN', '7807I9ygow9sLqNQuI7hsWPm5FLNfBr9c')
-#---------------------------------------------------------------
+API_ID = int(environ.get('API_ID', '32800910'))
+API_HASH = environ.get('API_HASH', '0cff9e94b5edf5ecd6dca783c21ae81b')
+BOT_TOKEN = environ.get('BOT_TOKEN', '8806666027:AAFzStoEuciMAwQYTEXEy49F0J4iy9n8bWQ')
+#---------------------------------------------------------BOT_TOKEN = environ.get('BOT_TOKEN', '7807I9ygow9sLqNQuI7hsWPm5FLNfBr9c')------
 #---------------------------------------------------------------
 ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '5016109398').split()]
 USERNAME = environ.get('USERNAME', "https://t.me/Innocent_babe_dead") # ADMIN USERNAME
